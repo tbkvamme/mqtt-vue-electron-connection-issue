28b5f9dbe52105b439ef894e55546e470dee1936
tbkvamme <blomholm@gmail.com>
2019-07-17 12:34:59 +0200

    Add example mqtt code for connection issue
    The example code is taken from mqtt.js github page:
    https://github.com/mqttjs/MQTT.js
    
    When electron app is starting up, the connection
    will not succeed. But when reloading the app/page
    it will succeed.
    
    This is very strange.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+var mqtt = require('mqtt')
+var brokerUrl = 'mqtt://test.mosquitto.org';
+
+console.log("Connecting to: " + brokerUrl);
+var client = mqtt.connect(brokerUrl)
+
+client.on('connect', function () {
+    console.log("Connected!");
+    client.subscribe('presence', function (err) {
+        if (!err) {
+            client.publish('presence', 'Hello mqtt')
+        }
+    })
+})
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
